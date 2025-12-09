20f2c5b8c41cb7b94a318ae357e1e1efc342abe3
Alexander Shatalkin <s.shatalkin@mail.ru>
2025-12-09 22:33:53 +0900

    completed mdn parser

---
+++ src/interface/ArtcileREcord.ts
@@ -0,0 +1,6 @@
+export interface ArticleRecord {
+    name: string;
+    local_path: string;
+    web_path: string;
+    source_id: number;
+}
--- src/parser/MDN.ts
+++ src/parser/MDN.ts
@@ -1,5 +1,7 @@
 import { t } from "elysia";
 import { MdFile, scanDirectory } from "../utils/scanDirectory";
+import { ArticleRecord } from "../interface/ArtcileREcord";
+import { prisma } from "../prisma/client";
 
 
 export interface MDNArticle {
@@ -11,21 +13,12 @@ export interface MDNArticle {
 
 function processMdFiles(files: MdFile[]): MDNArticle[] {
     return files.map(file => {
-        
         const normalized = file.path.replace(/\\/g, "/");
-
-        
         const withoutRoot = normalized.split("/").slice(1).join("/");
-
-        
         const withoutFile = withoutRoot.replace(/\/index\.md$/, "");
-
-        
         const parts = withoutFile.split("/");
         const route = parts.slice(1).join("/");
-
         const name = parts[parts.length - 1];
-
         return {
             fullPath: normalized,
             route: route,
@@ -34,7 +27,26 @@ function processMdFiles(files: MdFile[]): MDNArticle[] {
     });
 }
 
+function prepareArticle({ fullPath, route, name }: MDNArticle): ArticleRecord {
+    return {
+        name,
+        local_path: fullPath,
+        web_path: `https://developer.mozilla.org/${route}`,
+        source_id: 1
+    };
+}
+
+async function insertArticles(list: ArticleRecord[]) {
+    await prisma.article.createMany({
+        data: list,
+        skipDuplicates: true
+    });
+}
+
 export const parseMDN = async () => {
     const files = await scanDirectory("./docs/mdn");
-    console.log(processMdFiles(files));
+    const processedFiles = processMdFiles(files);
+    const articles  = processedFiles.map(f => prepareArticle(f));
+
+    await insertArticles(articles);
 }
