2e03a6921fb6b5513e2da9677b70cb616a07856d
Alexander Shatalkin <s.shatalkin@mail.ru>
2025-12-13 16:59:28 +0900

    data base chagees

--- src/interface/ArtcileREcord.ts
+++ src/interface/ArtcileREcord.ts
@@ -3,4 +3,6 @@ export interface ArticleRecord {
     local_path: string;
     web_path: string;
     source_id: number;
+    content: string;
+    content_type: string
 }
--- src/parser/MDN.ts
+++ src/parser/MDN.ts
@@ -2,37 +2,44 @@ import { t } from "elysia";
 import { MdFile, scanDirectory } from "../utils/scanDirectory";
 import { ArticleRecord } from "../interface/ArtcileRecord";
 import { prisma } from "../prisma/client";
-
+import { readFile } from "fs/promises";
 
 export interface MDNArticle {
     fullPath: string; 
     route: string;    
-    name: string;     
+    name: string;    
+    content: string; 
 }
 
 
-function processMdFiles(files: MdFile[]): MDNArticle[] {
-    return files.map(file => {
+async function processMdFiles(files: MdFile[]): Promise<MDNArticle[]> {
+    const promises = files.map(async (file) => {
         const normalized = file.path.replace(/\\/g, "/");
         const withoutRoot = normalized.split("/").slice(1).join("/");
         const withoutFile = withoutRoot.replace(/\/index\.md$/, "");
         const parts = withoutFile.split("/");
         const route = parts.slice(1).join("/");
         const name = parts[parts.length - 1];
+        const content = await readFile(file.path, "utf8");
         return {
             fullPath: normalized,
             route: route,
-            name: name
+            name: name,
+            content: content
         };
     });
+
+    return Promise.all(promises);
 }
 
-function prepareArticle({ fullPath, route, name }: MDNArticle): ArticleRecord {
+function prepareArticle({ fullPath, route, name, content }: MDNArticle): ArticleRecord {
     return {
         name,
         local_path: fullPath,
         web_path: `https://developer.mozilla.org/${route}`,
-        source_id: 1
+        source_id: 1,
+        content: content,
+        content_type: "md"
     };
 }
 
@@ -45,7 +52,7 @@ async function insertArticles(list: ArticleRecord[]) {
 
 export const parseMDN = async () => {
     const files = await scanDirectory("./docs/mdn");
-    const processedFiles = processMdFiles(files);
+    const processedFiles = await processMdFiles(files);
     const articles  = processedFiles.map(f => prepareArticle(f));
 
     await insertArticles(articles);
